8be994e614e0329c954161b6f905d5d784797aee
Philip Veremeichik <p.veremeichik@richbrains.net>
2026-01-14 13:21:27 +0300

    first commit

--- src/pages/map/ui/Building.tsx
+++ src/pages/map/ui/Building.tsx
@@ -34,7 +34,7 @@ export function Building({ src, alt = "", x, y, onClick }: BuildingProps) {
         onClick={onClick}
         onMouseEnter={mouseOn}
         onMouseLeave={mouseOut}
-        className={`absolute z-100 flex aspect-square w-30 -translate-x-1/2 -translate-y-1/2 cursor-pointer items-center justify-center rounded-full bg-red-500`}
+        className={`absolute z-100 flex aspect-square w-30 -translate-x-1/2 -translate-y-1/2 cursor-pointer items-center justify-center rounded-full`}
         style={{ left: `${x}%`, top: `${y}%` }}
       ></button>
     </div>
